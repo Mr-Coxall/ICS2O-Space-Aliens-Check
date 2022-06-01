124ea0eab1ac513d23f3b093ee9a230e57315297
MarcusW <99274214+MarcusWehbi@users.noreply.github.com>
2022-06-01 23:01:02 +0000

    Added game.js

---
+++ js/game.js
@@ -0,0 +1,11 @@
+/* global Phaser */
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x808080
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
