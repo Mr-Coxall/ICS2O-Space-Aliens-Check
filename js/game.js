e33f0ffc98522ca63aafecff01f3cd9e937955e2
MarcusW <99274214+MarcusWehbi@users.noreply.github.com>
2022-06-01 23:25:43 +0000

    Added comments to my file

--- js/game.js
+++ js/game.js
@@ -1,10 +1,29 @@
 /* global Phaser */
 
+// Copyright (c) 2022 Marcus Wehbi All rights reserved
+//
+// Created by: Marcus Wehbi
+// Created on: May 2022
+// This is the Phaser3 configuration file
+
+//* Game scene */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x808080
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false
+    }
+  },
+  // set background color
+  backgroundColor: 0x808080,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
