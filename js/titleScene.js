b71c6412eec45e9f46a1790b9fb4edf18f31ad23
MarcusW <99274214+MarcusWehbi@users.noreply.github.com>
2022-06-02 02:24:09 +0000

    Added splash image

--- js/titleScene.js
+++ js/titleScene.js
@@ -4,11 +4,11 @@
 //
 // Created by: Marcus Wehbi
 // Created on: May 2022
-// This is the Splash Scene
+// This is the Title Scene
 
-class SplashScene extends Phaser.Scene {
+class TitleScene extends Phaser.Scene {
   constructor () {
-    super({ key: 'splashScene' })
+    super({ key: 'titleScene' })
   }
 
   // get the scene up and running, initialize data object with content of another data object (our particular scene) 
@@ -18,23 +18,16 @@ class SplashScene extends Phaser.Scene {
 
   // handle asynchronous external file loading in a blocking manner
   preload () {
-    console.log('Splash Scene')
-    this.load.image('splashSceneBackground', './assets/splashSceneImage.png')
+    console.log('Title Scene')
   }
 
   // creating a new object by using an existing object as the prototype for the new object.
   create (data) {
-    this.splashSceneBackgroundImage = this.add.sprite(0, 0, 'splashSceneBackground')
-    this.splashSceneBackgroundImage.x = 1920 / 2
-    this.splashSceneBackgroundImage.y = 1080 / 2
   }
 
   // replacing old content of the element with new provided content, and returning the element. 
   update (time, delta) {
-    if (time > 3000){
-      this.scene.switch('titleScene')
-    }
   }  
 }
 
-export default SplashScene
+export default TitleScene
